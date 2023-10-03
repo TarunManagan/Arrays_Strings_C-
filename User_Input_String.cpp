ae0478d71d9536bd8b183f610955ef50a3feb544
TarunManagan <139469256+TarunManagan@users.noreply.github.com>
2023-10-03 14:56:55 +0530

    Create User_Input_String.cpp

---
+++ User_Input_String.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+
+int main() {
+    string name;
+    cout<<"Enter your name: ";
+    getline(cin,name);
+    cout<<"My name is: "<<name;
+    
+return 0;
+}
+/*OUTPUT
+Enter your name: Tarun
+My name is: Tarun
+*/
