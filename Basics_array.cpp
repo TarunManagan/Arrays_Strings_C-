259575154cfb3c9975f7fc81528127e867c97431
TarunManagan <139469256+TarunManagan@users.noreply.github.com>
2023-10-03 14:49:28 +0530

    Create Basics_array.cpp

---
+++ Basics_array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a1[5]={19,10,8,45,32};
+    for(int i=0; i<5; i++){
+        cout<<a1[i]<<endl;
+    }
+return 0;
+}
+
+/*OUTPUT
+19	10	8	45	32	
+*/
