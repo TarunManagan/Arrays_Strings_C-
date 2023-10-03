cae7af75c0e185563f0b14bfd1ad8b3bd0a208a1
TarunManagan <139469256+TarunManagan@users.noreply.github.com>
2023-10-03 17:21:02 +0530

    Update User_Input_String.cpp

--- User_Input_String.cpp
+++ User_Input_String.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     string name;
     cout<<"Enter your name: ";
-    getline(cin,name);
+    getline(cin,name); //getline is used to take input from the user // It even includes any " " in any string
     cout<<"My name is: "<<name;
     
 return 0;
