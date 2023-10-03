cba017da716bfe42d98bd8e037e762e22094ea10
TarunManagan <139469256+TarunManagan@users.noreply.github.com>
2023-10-03 16:45:09 +0530

    Create Matrx_AdditionOfDiagonal.cpp

---
+++ Matrx_AdditionOfDiagonal.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int r,c,sum=0,count=0;
+    cout<<"Enter the number of rows and columns of the matrix: ";
+    cin>>r>>c;
+    int A[r][c];
+    cout<<"Enter the elements of Matrix A: "<<endl;
+    for(int i=0; i<r;i++){
+        for(int j=0; j<c;j++){
+            cin>>A[i][j];
+        }
+    }
+    for(int i=0; i<r;i++){
+        for(int j=0; j<c;j++){
+            if(i==j){
+                sum = sum+A[i][j];
+            }else if(i+j==r-1){
+                count= count+A[i][j];
+            }
+            
+        }
+    }
+    cout<<"The sum of diagonal elements (from left to right) of Matrix A is: "<<sum<<endl;
+    cout<<"The sum of diagonal elements (from right to left) of Matrix A is: "<<count;
+    return 0;
+}
+/*OUPUT
+Enter the number of rows and columns of the matrix: 3 3
+Enter the elements of Matrix A: 
+1 2 3 4 5 6 7 8 9
+The sum of diagonal elements (from left to right) of Matrix A is: 15
+The sum of diagonal elements (from right to left) of Matrix A is: 10
+*/
