aa77961fe5e067d57e720856961e8b65cd190b27
TarunManagan <139469256+TarunManagan@users.noreply.github.com>
2023-10-03 14:52:51 +0530

    Create Sum_Avg_Array.cpp

---
+++ Sum_Avg_Array.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,sum=0;
+    float avg;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    int a1[n];
+    for(int i=0; i<n; i++){
+        cout<<"Enter the elements of the array: ";
+        cin>>a1[i];
+    }
+    for(int i=0; i<n; i++){
+        sum=sum+a1[i];
+        avg=sum/n;
+    }
+    cout<<"The sum of elements is: "<<sum<<endl;
+    cout<<"The average of elements is: "<<avg;
+return 0;
+}
+/*OUTPUT
+Enter the size of array: 5
+Enter the elements of the array: 1
+Enter the elements of the array: 2
+Enter the elements of the array: 3
+Enter the elements of the array: 4
+Enter the elements of the array: 5
+The sum of elements is: 15
+The average of elements is: 3	
+*/
