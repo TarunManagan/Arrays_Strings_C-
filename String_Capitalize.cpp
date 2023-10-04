b97c52c4198c8f1c03c2938f503a974365abb53a
TarunManagan <139469256+TarunManagan@users.noreply.github.com>
2023-10-05 02:27:25 +0530

    Update String_Capitalize.cpp

--- String_Capitalize.cpp
+++ String_Capitalize.cpp
@@ -8,7 +8,7 @@ int main() {
     getline(cin,str1);
     cout<<"The original string is: "<<str1<<endl;
     cout<<"The new string is: ";
-    for(int i=0; str1[i]!='\0'; i++){
+    for(int i=0; str1[i]!='\0'; i++){ //ASCII value A-Z is 65-90: Therefore subtract it from 32 as ASCII value a-z is 97-122
         cout<<char(str1[i]-32); //Converts all the lowercase to uppercase 
     }
 return 0;
